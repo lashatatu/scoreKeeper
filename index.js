ab03ff72457bcad1889b87ff17c68ebde93032d9
lashatatu <lashatatu@gmail.com>
2020-09-08 13:27:36 +0400

    1

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
+var resetButton = document.querySelector("#reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput=document.querySelector("input");
+var p=document.querySelector("p");
+var winningScore=document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
-let endScore = 5;
+var endScore = 5;
 
 
 p1Button.addEventListener("click", function () {
     if (!gameOver) {
         p1Score++;
         if (p1Score === endScore) {
+            p1Display.classList.add("winner");
             gameOver = true;
         }
         p1Display.textContent = p1Score;
@@ -22,8 +27,24 @@ p2Button.addEventListener("click", function () {
     if (!gameOver) {
         p2Score++;
         if (p2Score === endScore) {
+            p2Display.classList.add("winner");
             gameOver = true;
         }
         p2Display.textContent = p2Score;
     }
 });
+
+resetButton.addEventListener("click", function () {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+});
+
+numInput.addEventListener("change", function () {
+winningScore.textContent=numInput.value;
+endScore=Number(numInput.value);
+})
